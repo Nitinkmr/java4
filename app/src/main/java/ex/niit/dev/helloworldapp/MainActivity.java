cdd5c2c261546196f7853a34c3d3d0b8502364e0
Vasudev Kumaran <tech3info@gmail.com>
2018-07-05 16:06:33 +0530

    init

--- app/src/main/java/ex/niit/dev/helloworldapp/MainActivity.java
+++ app/src/main/java/ex/niit/dev/helloworldapp/MainActivity.java
@@ -2,20 +2,47 @@ package ex.niit.dev.helloworldapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("onCreate called");
+
+        resultView = (TextView) findViewById(R.id.resultView);
+        Button btn1 = (Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                MainActivity.this.resultView.setText("1");
+            }
+        });
+        Button btn2 = (Button)findViewById(R.id.btn2);
+        btn2.setOnClickListener(this);
+        Button btn3 = (Button)findViewById(R.id.btn3);
+        btn3.setOnClickListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         System.out.println("onResume called");
+    }
+
+    public void onBtnPressed(View view){
+        //
+        Button button = (Button)view;
+        if (button.getText().equals("4")){
+
+        }
     }
 
     @Override
@@ -34,5 +61,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         System.out.println("onDestroy called");
+    }
+
+    @Override
+    public void onClick(View v) {
+       // String text = resultView.getText().toString();
+        switch (v.getId()){
+            case R.id.btn2:
+                //
+                resultView.append("2");
+                break;
+
+            case R.id.btn3:
+                resultView.append("3");
+                break;
+        }
     }
 }
